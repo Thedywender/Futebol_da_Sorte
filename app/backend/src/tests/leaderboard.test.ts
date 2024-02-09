cb272f252d5c8bd1548dfb382c94cba2bd29be55
Thedywender <thedywender@hotmail.com>
2024-02-09 06:41:47 -0400

    tests cobrindo 96%

--- app/backend/src/tests/leaderboard.test.ts
+++ app/backend/src/tests/leaderboard.test.ts
@@ -4,17 +4,31 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import { leaderboardList } from './mocks/leaderboard.mocks';
+import { leaderboardListAll, leaderboardListAway, leaderboardListHome } from './mocks/leaderboard.mocks';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Testano leaderboards', () => {
+describe('Testando leaderboards Home', () => {
     it('testando o retorno da lista de leaderboard home', async function() {
         const { status, body } = await chai.request(app).get('/leaderboard/home');
 
         expect(status).to.equal(200);
-        expect(body).to.deep.equal(leaderboardList);
+        expect(body).to.deep.equal(leaderboardListHome);
+    })
+
+    it('Testando leaderboards away', async () => {
+        const { status, body } = await chai.request(app).get('/leaderboard/away');
+
+        expect(status).to.equal(200);
+        expect(body).to.deep.equal(leaderboardListAway);
+    })
+
+    it('Testando leaderboard getAll', async function() {
+        const { status, body } = await chai.request(app).get('/leaderboard');
+
+        expect(status).to.equal(200);
+        expect(body).to.deep.equal(leaderboardListAll);
     })
 })
